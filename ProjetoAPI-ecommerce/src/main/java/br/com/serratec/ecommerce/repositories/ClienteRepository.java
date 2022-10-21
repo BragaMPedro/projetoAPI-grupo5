e8911443435d2d18c415556a63d394e9ec36de15
GiseleBr <britto.gisele@gmail.com>
2022-10-21 16:38:40 -0300

    criando clienterepository

---
+++ ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package br.com.serratec.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.com.serratec.ecommerce.domains.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    
+
+}
