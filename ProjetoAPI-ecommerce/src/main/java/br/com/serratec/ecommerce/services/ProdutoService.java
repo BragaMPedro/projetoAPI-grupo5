f7fcf931ae698d505387ba901ef0aa47f85314e8
Isis_R_Castro <isis.reis.castro@gmail.com>
2022-10-22 20:23:45 -0300

    Adicionando DTO

--- ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/services/ProdutoService.java
+++ ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/services/ProdutoService.java
@@ -2,11 +2,13 @@ package br.com.serratec.ecommerce.services;
 
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collectors;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import br.com.serratec.ecommerce.domains.Produto;
+import br.com.serratec.ecommerce.dto.ProdutoRequestDTO;
+import br.com.serratec.ecommerce.dto.ProdutoResponseDTO;
 import br.com.serratec.ecommerce.exceptions.ResourceNotFoundException;
 import br.com.serratec.ecommerce.repositories.ProdutoRepository;
 
@@ -16,33 +18,50 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repositorio;
 	
-	public List<Produto> obterTodos() {
-		return repositorio.findAll();
+	private ModelMapper mapper = new ModelMapper();
+	
+	public List<ProdutoResponseDTO> obterTodos() {
+		List<Produto> lista = repositorio.findAll();
+
+		return lista.stream()
+				.map(produto -> mapper.map(produto, ProdutoResponseDTO.class))
+				.collect(Collectors.toList());
 	}
 	
-	public Optional<Produto> obterPorId(Long id_produto){
+	public Optional<ProdutoResponseDTO> obterPorId(Long id_produto){
 		
-		Optional<Produto> optProduto = repositorio.findById(id_produto);
+		var optProduto = repositorio.findById(id_produto);
 		
 		if(optProduto.isEmpty()) {
 			throw new ResourceNotFoundException("Não foi possivel encontrar o produto com id " + id_produto);
 		}
 		
-		return optProduto;
-	}
-	
-	public Produto cadastrar(Produto produto) {
+		ProdutoResponseDTO dto = mapper.map(optProduto.get(), ProdutoResponseDTO.class);
 		
-		produto.setId_produto(null);
-		return repositorio.save(produto);
+		return Optional.of(dto);
 	}
 	
-	public Produto atualizar(Long id_produto, Produto produto) {
+	public ProdutoResponseDTO cadastrar(ProdutoRequestDTO produto ) {
+		
+		var produtoModel = mapper.map(produto, Produto.class);
+
+		repositorio.save(produtoModel);
+        
+        var response = mapper.map(produtoModel, ProdutoResponseDTO.class);
+        return response;
+	}
+	
+	public ProdutoResponseDTO atualizar(Long id_produto, ProdutoRequestDTO produto) {
 		
 		obterPorId(id_produto);
 		
-		produto.setId_produto(id_produto);
-		return repositorio.save(produto);
+		var produtoModel = mapper.map(produto, Produto.class);
+		
+		produtoModel.setId_produto(id_produto);
+		repositorio.save(produtoModel);
+		
+		return mapper.map(produtoModel, ProdutoResponseDTO.class);
+		
 	}
 	
 	public void deletar(Long id) {
