0a3b2cad90e70b7f6a3652ce22847a1ec208a564
MatheusCarius <matheus_julianoc@hotmail.com>
2022-10-22 18:57:15 -0300

    Adicionando PedidoService

--- ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/domains/Pedido.java
+++ ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/domains/Pedido.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
