0efb1cb94f6df279e1598cb362a462e728270b5a
MatheusCarius <matheus_julianoc@hotmail.com>
2022-10-23 10:32:39 -0300

    Adicionando getters and setters para ItemPedido

--- ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/domains/ItemPedido.java
+++ ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/domains/ItemPedido.java
@@ -45,4 +45,68 @@ public class ItemPedido {
 	@JoinColumn(name="id_produto")
 	private Produto produto;
 
+    public Long getId_itemPedido() {
+        return id_itemPedido;
+    }
+
+    public void setId_itemPedido(Long id_itemPedido) {
+        this.id_itemPedido = id_itemPedido;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco_venda() {
+        return preco_venda;
+    }
+
+    public void setPreco_venda(double preco_venda) {
+        this.preco_venda = preco_venda;
+    }
+
+    public double getPercentual_desconto() {
+        return percentual_desconto;
+    }
+
+    public void setPercentual_desconto(double percentual_desconto) {
+        this.percentual_desconto = percentual_desconto;
+    }
+
+    public double getValor_bruto() {
+        return valor_bruto;
+    }
+
+    public void setValor_bruto(double valor_bruto) {
+        this.valor_bruto = valor_bruto;
+    }
+
+    public double getValor_liquido() {
+        return valor_liquido;
+    }
+
+    public void setValor_liquido(double valor_liquido) {
+        this.valor_liquido = valor_liquido;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
 }
