649a9807720c86bb9562f6c61b4a1d10c89bcada
Isis_R_Castro <isis.reis.castro@gmail.com>
2022-10-23 00:53:45 -0300

    Adicionando Email

--- ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/services/ClienteService.java
+++ ProjetoAPI-ecommerce/src/main/java/br/com/serratec/ecommerce/services/ClienteService.java
@@ -1,5 +1,6 @@
 package br.com.serratec.ecommerce.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -7,6 +8,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.serratec.ecommerce.domains.Cliente;
+import br.com.serratec.ecommerce.domains.MensagemEmail;
 import br.com.serratec.ecommerce.dto.ClienteRequestDTO;
 import br.com.serratec.ecommerce.dto.ClienteResponseDTO;
 import br.com.serratec.ecommerce.exceptions.ResourceNotFoundException;
@@ -17,6 +19,9 @@ public class ClienteService {
 
     @Autowired
     private ClienteRepository repositorio;
+    
+    @Autowired
+    private MailService emailService;
     
     private ModelMapper mapper = new ModelMapper();
     
@@ -47,6 +52,18 @@ public class ClienteService {
 
 		repositorio.save(clienteModel);
 		var response = mapper.map(clienteModel, ClienteResponseDTO.class);
+		
+		var destinatarios = new ArrayList<String>();
+        destinatarios.add("turma05serratec@gmail.com");
+        destinatarios.add("pedrobmagalhaes95@gmail.com");
+
+        MensagemEmail email = new MensagemEmail(
+            "Nova conta criada.",
+            "<h1 style=\"color:red\"> Conta criada com Sucesso! </h1>",
+            "turma05serratec@gmail.com",
+            destinatarios);
+        
+        emailService.enviarEmail(email);
         
         return response;
     }
